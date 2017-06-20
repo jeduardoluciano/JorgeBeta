23194b44934837790f36e6b716846149bf7d8891
Jorge Eduardo <jeolcavaco@hotmail.com>
2017-06-20 00:28:54 -0300

    plugin de teste

---
+++ src/ios/JorgeBeta.m
@@ -0,0 +1,39 @@
+/********* JorgeBeta.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface JorgeBeta : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation JorgeBeta
+
+- (void)getNextAppointment:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    int minutes = [[command.arguments objectAtIndex:0] intValue];
+
+    if (minutes > 0) {
+        /**
+            Code to retrieve name, time of next appointment goes here
+        **/
+        if (foundAppointment) {
+            NSMutableDictionary *appointment = [NSMutableDictionary dictionaryWithCapacity:2];
+            [appointment setObject:appointmentName forKey:@"title"];
+            [appointment setObject:appointmentDate forKey:@"date"];
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:appointment];
+        } else {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
+        }
+    } else {
+        // Indicate an error result
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
